bd1671f57bfcd3588804a7288962f47a8cced4b6
Jhair Lozano <jhair.lozano@vasslatam.com>
2020-02-10 00:07:01 -0500

    Brujula básica

---
+++ app/src/main/java/com/belatrix/brujula/ui/CompassActivity.kt
@@ -0,0 +1,13 @@
+package com.belatrix.brujula.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.belatrix.brujula.R
+
+class CompassActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compass)
+    }
+}
